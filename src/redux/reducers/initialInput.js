415d05f6ced63b76903bb0a3e421e3c6dbf1bac8
jywillia <john.yong.williams@gmail.com>
2020-04-29 15:11:38 -0400

    updated css and initial state

--- src/redux/reducers/initialInput.js
+++ src/redux/reducers/initialInput.js
@@ -1,27 +1,28 @@
 // export initial state const for first load
-export const initialInput = `# Welcome to my React Markdown Previewer!
+export const initialInput = 
+`# Welcome to my React Markdown Previewer!
 
 ## This is a sub-heading...
-### And here's some other cool stuff:
-  
+
+### And here's some other cool stuff:  
+
 Heres some code, \`<div></div>\`, between 2 backticks.
 
 \`\`\`
 // this is multi-line code:
-
 function anotherExample(firstLine, lastLine) {
   if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
     return multiLineCode;
   }
 }
-\`\`\`
-  
+\`\`\`  
+
 You can also make text **bold**... whoa!
 Or _italic_.
 Or... wait for it... **_both!_**
 And feel free to go crazy ~~crossing stuff out~~.
-
 There's also [links](https://www.freecodecamp.com), and
+
 > Block Quotes!
 
 And if you want to get really crazy, even tables:
@@ -36,12 +37,10 @@ And here. | Okay. | I think we get it.
      - With different indentation levels.
         - That look like this.
 
-
 1. And there are numbererd lists too.
 1. Use just 1s if you want! 
 1. But the list goes on...
 - Even if you use dashes or asterisks.
 * And last but not least, let's not forget embedded images:
 
-![React Logo w/ Text](https://goo.gl/Umyytc)
-`;
+![React Logo w/ Text](https://goo.gl/Umyytc)`;
